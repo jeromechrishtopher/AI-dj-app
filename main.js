dcc85c9033042bc1a4621f156b704b0ab55e7b70
jeromechrishtopher <85341703+jeromechrishtopher@users.noreply.github.com>
2021-12-04 11:43:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -34,7 +34,36 @@ function draw(){
         document.getElementById("volume").innerHTML="volume-"+volume;
         song.setVolume(volume);
     }
-    
+        if(scoreRightWrist>0.2){
+            circle(rightWristX,rightWristY,20);
+            if(rightWristY>0 && rightWristY<=100){
+                document.getElementById("speed").innerHTML="speed=0.5x";
+                song.rate(0.5);
+
+            }
+            else  if(rightWristY>100 && rightWristY<=200){
+                document.getElementById("speed").innerHTML="speed=1x";
+                song.rate(1);
+
+            }
+            else  if(rightWristY>200 && rightWristY<=300){
+                document.getElementById("speed").innerHTML="speed=1.5x";
+                song.rate(1.5);
+
+
+            }
+            else  if(rightWristY>300 && rightWristY<=400){
+                document.getElementById("speed").innerHTML="speed=2x";
+                song.rate(2);
+
+            }
+            else  if(rightWristY>400 && rightWristY<=500){
+                document.getElementById("speed").innerHTML="speed=2.5x";
+                song.rate(2.5);
+                
+
+            }
+        }
     
 }
 
@@ -43,6 +72,8 @@ function gotPoses(results){
         console.log(results);
         scoreLeftWrist=results[0].pose.keypoints[9].score;
         scoreRightWrist=results[0].pose.keypoints[10].score;
+        console.log("scorerightwrist="+scoreRightWrist+"scoreleftwrist="+scoreLeftWrist);
+
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         rightWristX=results[0].pose.rightWrist.x;
